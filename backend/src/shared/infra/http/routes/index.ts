415ab7c21fc29951d38810af26c37b11f90364ff
Leandher Bessa de Almeida <leandher_bessa@hotmail.com>
2020-06-03 14:18:04 -0300

    chore(): remove unused methods

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express'
 
 import deliveryManRouter from '@modules/deliveryman/infra/http/routes/deliveryman.routes'
 import recipientsRouter from '@modules/recipients/infra/http/routes/recipients.routes'
-import profilesRouter from '@modules/users/infra/http/routes/profiles.routes'
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes'
 import usersRouter from '@modules/users/infra/http/routes/users.routes'
 
@@ -10,7 +9,6 @@ const routes = Router()
 
 routes.use('/users', usersRouter)
 routes.use('/sessions', sessionsRouter)
-routes.use('/profile', profilesRouter)
 routes.use('/recipients', recipientsRouter)
 routes.use('/deliveryman', deliveryManRouter)
 
